ad10a91216454e692f2c8b38e66c33b9e893b864
Alexandre Capt <acapt@adobe.com>
2022-03-21 10:24:20 +0100

    wip

---
+++ README.md
@@ -0,0 +1,3 @@
+# Helix Importer tool
+
+WIP
---
+++ js/pollimporter.js
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/* global WebImporter */
+
+export default class PollImporter {
+  constructor(cfg) {
+    this.config = {
+      importFileURL: 'http://localhost:3000/tools/importer/import.js',
+      poll: true,
+      ...cfg,
+    };
+    this.poll = this.config.poll;
+
+    this.init();
+  }
+
+  async init() {
+    const $this = this;
+    const loadModule = async (projectTransformFileURL) => {
+      try {
+        const mod = await import(`${projectTransformFileURL}?cf=${new Date().getTime()}`);
+        if (mod.default) {
+          this.projectTransform = mod.default;
+        }
+      } catch (err) {
+        console.warn('failed to load project transform module', err);
+      }
+    };
+    const poll = async () => {
+      const projectTransformFileURL = this.config.importFileURL;
+      try {
+        const res = await fetch(projectTransformFileURL);
+        const body = await res.text();
+
+        if (body !== $this.lastProjectTransformFileBody) {
+          $this.lastProjectTransformFileBody = body;
+          await loadModule(projectTransformFileURL);
+          if ($this.transformationURL && $this.transformationDocument) {
+            $this.transform();
+          }
+        }
+      } catch (err) {
+        if ($this.lastProjectTransformFileBody !== 'nofilefound') {
+          console.warn('failed to poll project transform module', err);
+          $this.lastProjectTransformFileBody = 'nofilefound';
+          if ($this.transformationURL && $this.transformationDocument) {
+            $this.transform();
+          }
+        }
+      }
+    };
+
+    if (!this.projectTransformInterval) {
+      await poll();
+      this.projectTransformInterval = setInterval(poll, 5000);
+    }
+  }
+
+  async transform(includeDocx = false) {
+    let out;
+    if (includeDocx) {
+      out = await WebImporter.html2docx(
+        this.transformationURL,
+        this.transformationDocument,
+        this.projectTransform,
+      );
+
+      const { path } = out;
+      out.filename = `${path}.docx`;
+    } else {
+      out = await WebImporter.html2md(
+        this.transformationURL,
+        this.transformationDocument,
+        this.projectTransform,
+      );
+    }
+
+    return out;
+  }
+
+  setTransformationInput({ url, document }) {
+    this.transformationURL = url;
+    this.transformationDocument = document;
+  }
+}
